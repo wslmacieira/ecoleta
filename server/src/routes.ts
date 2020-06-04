967281b8065d438b49c3c802d164ac6079baae97
wslmacieira <wslmacieira@outlook.com>
2020-06-04 09:55:43 -0300

    Criando points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -17,4 +17,50 @@ routes.get('/items', async (request, response) => {
   return response.json(serializedItems);
 });
 
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const point = {
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  }
+
+  const insertedIds = await trx('points').insert(point);
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    }
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  await trx.commit();
+
+  return response.json({
+    id: point_id,
+    ...point
+  });
+});
+
 export default routes;
